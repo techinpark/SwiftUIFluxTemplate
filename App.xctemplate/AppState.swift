af15588c89c3f45ec0fec3d08e56fc21ff637a28
Fernando <fernando@kakao.com>
2020-11-02 08:09:50 +0900

    Initial commit 🎉

---
+++ App.xctemplate/AppState.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//
+
+import Foundation 
+import SwiftUIFlux 
+
+struct AppState: FluxState, Codable {
+    
+    // MARK: App State 
+    
+    // var yourState: YourState
+    init() {
+        // self.yourState = YourState() 
+    }
+
+}
